079c1512fa74ef9146444d525fb92822ee8e869b
YiXiu <yixiu17@gmail.com>
2018-11-20 17:37:05 -0500

    Wrote test cases for function either

--- HTML parser/parsing_test.rkt
+++ HTML parser/parsing_test.rkt
@@ -2,7 +2,6 @@
 (require "parsing.rkt")
 (require rackunit)
 
-; TODO: WRITE TESTS!!
 ;testcases for function parse-html-tag
 (check-equal? (parse-html-tag "<html></html>") '("<html>" "</html>") "Basic case")
 (check-equal? (parse-html-tag "<html>") '("<html>" "") "Extreme case: empty end text")
@@ -15,18 +14,23 @@
 (check-equal? (parse-html-tag "") '(error "") " Error case: no head")
 (check-equal? (parse-html-tag "ab") '(error "ab") "Error case: no head")
 
+
 ;testcases for function make-text-parser
 (define parse-hi (make-text-parser "hi"))
 (check-equal? (parse-hi "hiya!") '("hi" "ya!") "Test case")
 (check-equal? (parse-hi "h") '(error "h") "Error case: wrong header")
+
 (define parse-empty (make-text-parser ""))
 (check-equal? (parse-empty "") '("" "") "Extreme case: empty header")
 (check-equal? (parse-empty "abc") '("" "abc") "Extreme case: empty header")
+
 (define parse-324 (make-text-parser "324"))
 (check-equal? (parse-324 "csc324 is best") '(error "csc324 is best") "Error case: wrong header")
+
 (define parse-space (make-text-parser "  "))
 (check-equal? (parse-324 " 1") '(error " 1") "Error case: wrong header")
 (check-equal? (parse-space "  11  ") '("  " "11  ") "Special case: space character")
+
 
 ;testcases for fuction parse-non-special-char
 (check-equal? (parse-non-special-char "hi") '(#\h "i") "Normal text")
@@ -38,6 +42,7 @@
 (check-equal? (parse-non-special-char "=5") '(error "=5") "Error case: special char =")
 (check-equal? (parse-non-special-char "/say what") '(error "/say what") "Error case: special char /")
 
+
 ;testcases for fuction parse-plain-char
 (check-equal? (parse-plain-char "bbc") '(#\b "bc") "Normal text")
 (check-equal? (parse-plain-char ">tell me tell me") '(error ">tell me tell me")
@@ -45,11 +50,29 @@
 (check-equal? (parse-plain-char "a=") '(#\a "=") "Normal text")
 (check-equal? (parse-plain-char " white space error") '(error " white space error")
               "Error case: white space")
-(check-equal? (parse-plain-char "") '(error "") "Error case: empty text")
+;(check-equal? (parse-plain-char "") '(error "") "Error case: empty text")
 (check-equal? (parse-plain-char "\"") '(error "\"") "Error case: special char")
 
 
-
+;testcases for fuction either
+(check-equal? ((either parse-html-tag parse-non-special-char)"<html> good <html>") '("<html>" " good <html>")
+              "Test <html> tag")
+(check-equal? ((either parse-html-tag parse-non-special-char)"<html good <html>") '(error "<html good <html>")
+              "Error case: both parser failed")
+(check-equal? ((either parse-html-tag parse-non-special-char)"html good <html>") '(#\h "tml good <html>")
+              "Test 2nd parser")
+;self defined parser
+(define parse-abc (make-text-parser "abc"))
+(check-equal? ((either parse-abc parse-plain-char)"abc") '("abc" "") "Test 1st parser")
+(check-equal? ((either parse-abc parse-plain-char)"ab") '(#\a "b") "Test 2nd parser")
+(check-equal? ((either parse-abc parse-plain-char)"<ab") '(error "<ab") "Error case: both parser failed")
+;self defined parser - special character
+(define parse-bracket (make-text-parser "<"))
+(check-equal? ((either parse-abc parse-bracket)"<ab") '("<" "ab") "Test 2nd parser")
+(check-equal? ((either parse-plain-char parse-bracket)"<ab") '("<" "ab") "Test 2nd parser")
+(check-equal? ((either parse-non-special-char parse-plain-char)" call 911") '(#\space "call 911") "Test 1st parser")
+(check-equal? ((either parse-plain-char parse-non-special-char)" call 911") '(#\space "call 911") "Test 2nd parser")
+(check-equal? ((either parse-plain-char parse-non-special-char)"=") '(error "=") "Error case: special char")
 
 
 
